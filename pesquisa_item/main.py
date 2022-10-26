036d69e1f6ea15767561df676f704a8ff9e43a86
William <willianch3@gmail.com>
2022-10-25 21:43:38 -0300

    Commit exercicios

--- pesquisa_item/main.py
+++ pesquisa_item/main.py
@@ -6,21 +6,15 @@ vendas = {1:{'Produto':'Televisor 55', 'Quantidade': 5, 'Valor':3.500, 'Total':1
           2:{'Produto':'Batedeira', 'Quantidade': 10, 'Valor':350, 'Total':3500},
           3:{'Produto':'celular', 'Quantidade': 17, 'Valor':1.500, 'Total':25.500},
           4:{'Produto':'liquidificador', 'Quantidade': 3, 'Valor':280, 'Total':840},
-          5:{'Produto':'notebook', 'Quantidade': 7, 'Valor':3.500, 'Total':24.500},}
+          5:{'Produto':'notebook', 'Quantidade': 7, 'Valor':3.500, 'Total':24.500},
+        }
 
 
 app = FastAPI()
 
 @app.get('/')
 async def index():
-    return {'Mensagem': 'Bem vindo!!'}
-
-@app.get('/vendas/{item_id}')
-async def get_vendas(item_id: int):
-    if item_id in vendas:
-        return vendas[item_id]
-    else:
-        raise HTTPException(status_code=404, detail='Item not found.')
+    return {'Mensagem':'Seja bem vindo!'}
 
 
 class Item(BaseModel):
@@ -30,13 +24,8 @@ class Item(BaseModel):
     Total: float
 
 
-@app.post("/items/")
+@app.post('/items/')
 async def create_item(item: Item):
     vendas[len(vendas) + 1] = {
-            'Produto': item.Produto,
-            'Quantidade': item.Quantidade,
-            'Valor': item.Valor,
-            'Total': item.Quantidade * item.Valor
-    }
-    return item
 
+    }
